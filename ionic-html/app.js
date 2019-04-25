ff46c3b201ecbd2187ea8ce672a0841b3bfe2a12
Pete Davis <oldmanplanet@gmail.com>
2019-04-25 21:12:56 +0100

    ionic-html

---
+++ ionic-html/app.js
@@ -0,0 +1,6 @@
+const itemEl = document.getElementById("item");
+const amountEl = document.getElementById("amount");
+const addEl = document.getElementById("btn-add");
+const clearEl = document.getElementById("btn-clear");
+
+console.log(itemEl);
